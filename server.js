075e556b8b7b41459c038be4efe3e67e4c153661
henlovelybird <voxylophone@gmail.com>
2020-01-20 13:03:01 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const cors = require("cors")
+const server = express()
+const mongoose = require ("mongoose")
+const listEndpoints = require("express-list-endpoints")
+
+
+mongoose.connect(process.env.mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(db => console.log("MongoDB Connected"))
+  .catch(err => console.log("ERROR connecting to MongoDb", err));
+ 
+
+server.use(express.json())
+server.use(cors())
+server.get("/", async (req, res) => {
+    res.send("server is working")
+})
+
+
+const port = process.env.PORT || 7001
+
+console.log(listEndpoints(server));
+
+server.listen(port, () => {
+    console.log(`Your server is running on port ${port}`)
+})
