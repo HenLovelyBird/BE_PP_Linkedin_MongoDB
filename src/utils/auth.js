95db172afecca177d03cf72086a9289eaba15466
henlovelybird <voxylophone@gmail.com>
2020-02-14 16:42:59 +0100

    adding authorization to profile

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,7 @@
 const LocalStrategy = require("passport-local") // strategy to verify username and password
 const passport = require("passport")
 const User = require("../models/userSchema")
+const Profile = require('../models/profileSchema')
 
 //token stuff:
 const jwt = require("jsonwebtoken")
@@ -14,7 +15,8 @@ dotenv.config()
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
-passport.use(new LocalStrategy(User.authenticate())) // this strategy will be used when we ask passport to passport.authenticate("local")
+passport.use(new LocalStrategy(User.authenticate())) // this strategy will be used when we ask passport 
+// to passport.authenticate("local")
 
 //-----------------JWT AREA-------------------------------------
 const jwtOptions = {
@@ -24,16 +26,27 @@ const jwtOptions = {
 
 passport.use(new JwtStrategy
     (jwtOptions, 
-        (jwtPayload, callback) =>{ //this strategy will be used when we ask passport to passport.authenticate("jwt")
+            (jwtPayload, callback) =>{ //this strategy will be used when we ask passport to passport.authenticate("jwt")
             User.findById(jwtPayload._id, (err, user) => { //looks into the collection
             if (err) return callback(err, false) // ==> Something went wrong getting the info from the db
             else if (user) return callback(null, user) // ==> Existing user, all right!
             else return callback(null, false) // ==> Non existing user
     })
 }))
+
+passport.use(new JwtStrategy
+    (jwtOptions, 
+            (jwtPayload, callback) =>{ //this strategy will be used when we ask passport to passport.authenticate("jwt")
+            Profile.findById(jwtPayload._id, (err, user) => { //looks into the collection
+            if (err) return callback(err, false) // ==> Something went wrong getting the info from the db
+            else if (user) return callback(null, user) // ==> Existing user, all right!
+            else return callback(null, false) // ==> Non existing user
+    })
+}))
+
 //-----------------JWT AREA-------------------------------------
 
 module.exports = {
-    getToken: (user) => jwt.sign(user, jwtOptions.secretOrKey, { expiresIn: 1000 }) 
+    getToken: (user) => jwt.sign(profile, user, jwtOptions.secretOrKey, { expiresIn: 1000 }) 
     //this is just a helper to have a central point for token generation
 }
